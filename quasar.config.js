c645e42e6a0f9cfbc9c5c17c9bc9ddbd07e42716
Jirayu48 <tvkingdragon@gmail.com>
2025-10-08 10:45:16 +0700

    Edit quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -44,7 +44,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/6704101317-Jirayu-Vanasin/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
